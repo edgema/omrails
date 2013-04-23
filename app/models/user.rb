87a21b293abb4c72ba4f4e0bd6235c2eb5d3ef3b
Derek LaPorte <derek@printedtime.com>
2013-04-23 08:05:21 -0500

    removed forgot password

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,8 +3,8 @@ class User < ActiveRecord::Base
   # :token_authenticatable, :confirmable,
   # :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
-
+         :rememberable, :trackable, :validatable
+#:recoverable to be added later (Forgot password functionality)
   # Setup accessible (or protected) attributes for your model
   attr_accessible :email, :password, :password_confirmation, :remember_me, :name
   # attr_accessible :title, :body
