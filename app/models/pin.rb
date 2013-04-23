5f3aa597997ee6cd42b026588ed13402a81b4bcd
Derek LaPorte <derek@printedtime.com>
2013-04-23 08:37:17 -0500

    Add Pins scaffold

---
+++ app/models/pin.rb
@@ -0,0 +1,5 @@
+class Pin < ActiveRecord::Base
+  attr_accessible :description
+
+  validates :description, presence: true, length: { less_than: 50 }
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Omrails::Application.routes.draw do
  
+  resources :pins
+
+
   devise_for :users
 
  get 'about' => 'pages#about'
