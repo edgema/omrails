326dbf17f107bb123e80c626bdee42c27b840ebb
Derek LaPorte <derek@printedtime.com>
2013-04-23 22:17:14 -0500

    paperclip gem

--- app/models/pin.rb
+++ app/models/pin.rb
@@ -1,8 +1,13 @@
 class Pin < ActiveRecord::Base
-  attr_accessible :description
+  attr_accessible :description, :image
+
 
   validates :description, presence: true, :length => { :maximum => 177 }
-
-  belongs_to :user
   validates :user_id, presence: true
+  validates_attachment :image, presence: true,
+  						content_type: { content_type: ['image/jpeg', 'image/jpg', 'image/png', 'image/gif']},
+  						size: { less_than: 3.megabytes }
+  belongs_to :user
+  has_attached_file :image, styles: { medium: "320x240"}
+  
 end
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -37,4 +37,5 @@ Omrails::Application.configure do
 
 # In production, :host should be set to the actual host of your application.
   config.action_mailer.default_url_options = { :host => 'localhost:3000' }
+  Paperclip.options[:command_path] = "/opt/ImageMagick/bin/"
 end
