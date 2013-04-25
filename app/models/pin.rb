ef3095da0bc67dbeaf13c87f7be0ea61a2315787
Derek LaPorte <derek@printedtime.com>
2013-04-24 21:15:56 -0500

    Add image uploading via URL

--- app/models/pin.rb
+++ app/models/pin.rb
@@ -1,13 +1,19 @@
 class Pin < ActiveRecord::Base
-  attr_accessible :description, :image, :url
+  attr_accessible :description, :image, :url, :image_remote_url
 
 
   validates :description, presence: true, :length => { :maximum => 177 }
   validates :user_id, presence: true
+  validates :url, presence: true
   validates_attachment :image, presence: true,
   						content_type: { content_type: ['image/jpeg', 'image/jpg', 'image/png', 'image/gif']},
   						size: { less_than: 3.megabytes }
   belongs_to :user
   has_attached_file :image, styles: { medium: "320x240"}
+
   
+def image_remote_url=(url_value)
+	self.image = URI.parse(url_value) unless url_value.blank?
+	super
+
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130424191429) do
+ActiveRecord::Schema.define(:version => 20130425020516) do
 
   create_table "pins", :force => true do |t|
     t.string   "description"
@@ -23,6 +23,7 @@ ActiveRecord::Schema.define(:version => 20130424191429) do
     t.integer  "image_file_size"
     t.datetime "image_updated_at"
     t.string   "url"
+    t.string   "image_remote_url"
   end
 
   add_index "pins", ["user_id"], :name => "index_pins_on_user_id"
